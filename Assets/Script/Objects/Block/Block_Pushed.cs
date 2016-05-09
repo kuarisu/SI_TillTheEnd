74b6638d789716b98427db19a47eb1b0fa30e16a
kuarisu <kuarisuran@gmail.com>
2016-05-09 16:52:19 +0200

    changing block pushed script

--- Assets/Script/Objects/Block/Block_Pushed.cs
+++ Assets/Script/Objects/Block/Block_Pushed.cs
@@ -9,15 +9,11 @@ public class Block_Pushed : MonoBehaviour {
       
         if (col.tag == "Player")
         {
+            //Use two points to make a vector and push the block in the opposite direction
             Debug.Log("Hallo");
             Vector3 _positionTarget = col.transform.position;
             Vector3 _direction = (transform.position - _positionTarget).normalized * ExplosionStrengh;
             GetComponent<Rigidbody>().AddForce(_direction);
-
-            //Rigidbody _Rb = col.transform.parent.GetComponent<Rigidbody>();
-            //Vector3 forceVec = -_Rb.velocity.normalized * ExplosionStrengh;
-            //Debug.Log(_Rb.velocity);
-            //GetComponent<Rigidbody>().AddForce(forceVec, ForceMode.Acceleration);
 
         }
     }
